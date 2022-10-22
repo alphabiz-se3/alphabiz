a1e26a7862fd3e2d154390bb5e98ede389215942
alphabiz-se3 <se3.dev@alpha.biz>
2022-10-22 14:43:12 +0800

    restore debug assets

--- developer/update.js
+++ developer/update.js
@@ -1,1 +1,18 @@
-module.exports={github:{username:"tanshuai",repo:"alphabiz",internalRepo:"alphabiz-app"},bucketUrl:"https://s3.amazonaws.com/internal.alpha.biz",s3DownloadUrl:"https://d2v5t3td4po4es.cloudfront.net/releases/"};
+module.exports = {
+  /**
+   * Your github config, used for feedback, upgrade, etc
+   */
+  github: {
+    username: 'tanshuai',
+    repo: 'alphabiz',
+    /**
+     * Internal build repo
+     */
+    internalRepo: 'alphabiz-app'
+  },
+  /**
+   * Amazon S3 bucket for your app
+   */
+  bucketUrl: 'https://s3.amazonaws.com/internal.alpha.biz',
+  s3DownloadUrl: 'https://d2v5t3td4po4es.cloudfront.net/releases/'
+}
